771631d8bd87df59184fb5d7f5897234b35dcc11
oleksandr.popov <oleksandr.popov@onlini.co>
2017-02-16 16:59:17 +0200

    Added rendering background vignette.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ requirejs.config({
     'baseUrl': 'js/app',
     'paths': {
       'app': '../app',
-      'jquery': '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min'
+      'jquery': '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
+      'bootstrap-slider': '//cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.7.2/bootstrap-slider.min.js'
     }
 });
 
--- js/app/FurRenderer.js
+++ js/app/FurRenderer.js
@@ -3,7 +3,6 @@
 define([
         'framework/BaseRenderer',
         'jquery',
-        'LMTableShader',
         'FurShader',
         'DiffuseShader',
         'DiffuseColoredShader',
@@ -11,12 +10,12 @@ define([
         'framework/FullModel',
         'framework/UncompressedTextureLoader',
         'framework/CompressedTextureLoader',
-        'FurPresets'
+        'FurPresets',
+        'VignetteData'
     ],
     function(
         BaseRenderer,
         $,
-        LMTableShader,
         FurShader,
         DiffuseShader,
         DiffuseColoredShader,
@@ -24,7 +23,8 @@ define([
         FullModel,
         UncompressedTextureLoader,
         CompressedTextureLoader,
-        FurPresets) {
+        FurPresets,
+        VignetteData) {
 
         class FurRenderer extends BaseRenderer {
             constructor() {
@@ -40,6 +40,9 @@ define([
 
                 this.currentPreset = null;
                 this.nextPreset = null;
+
+                this.matOrtho = MatrixUtils.mat4.create();
+                MatrixUtils.mat4.ortho(this.matOrtho, -1, 1, -1, 1, 2.0, 250);
 
                 this.ITEMS_TO_LOAD = 4; // total number of OpenGL buffers+textures to load
                 this.FLOAT_SIZE_BYTES = 4; // float size, used to calculate stride sizes
@@ -77,10 +80,9 @@ define([
             }
 
             initShaders() {
-                this.shaderLMTable = new LMTableShader();
-                this.shaderFur = new FurShader();
                 this.shaderDiffuse = new DiffuseShader();
                 this.shaderDiffuseColored = new DiffuseColoredShader();
+                this.shaderFur = new FurShader();
             }
 
             /**
@@ -113,9 +115,16 @@ define([
 
                 this.modelCube = new FullModel();
                 this.modelCube.load('data/models/box10_rounded', boundUpdateCallback);
-                this.textureChecker = UncompressedTextureLoader.load('data/textures/checker.png', boundUpdateCallback);
+                // this.textureChecker = UncompressedTextureLoader.load('data/textures/checker.png', boundUpdateCallback);
+                this.textureBackground = UncompressedTextureLoader.load('data/textures/bg-gradient.png', boundUpdateCallback);
 
-                this.loadFurData(boundUpdateCallback);
+                this.currentPreset = FurPresets.current();
+
+                this.textureFurDiffuse = UncompressedTextureLoader.load('data/textures/' + this.getCurrentPresetParameter('diffuseTexture'), boundUpdateCallback);
+                this.textureFurAlpha = UncompressedTextureLoader.load('data/textures/' + this.getCurrentPresetParameter('alphaTexture'), boundUpdateCallback);
+
+                this.vignette = new VignetteData();
+                this.vignette.initGL(gl);
             }
 
             getCurrentPresetParameter(param) {
@@ -124,22 +133,6 @@ define([
 
             getNextPresetParameter(param) {
                 return this.nextPreset[param];
-            }
-
-            loadFurData(callback) {
-                this.currentPreset = FurPresets.current();
-
-                this.textureFurDiffuse && gl.deleteTexture(this.textureFurDiffuse);
-                this.textureFurAlpha && gl.deleteTexture(this.textureFurAlpha);
-
-                this.textureFurDiffuse = UncompressedTextureLoader.load('data/textures/' + this.getCurrentPresetParameter('diffuseTexture'), callback);
-                this.textureFurAlpha = UncompressedTextureLoader.load('data/textures/' + this.getCurrentPresetParameter('alphaTexture'), callback);
-
-                this.furThickness = this.getCurrentPresetParameter('thickness');
-                this.furLayers = this.getCurrentPresetParameter('layers');
-                this.furStartColor = this.getCurrentPresetParameter('startColor');
-                this.furEndColor = this.getCurrentPresetParameter('endColor');
-                this.furWaveScale = this.getCurrentPresetParameter('waveScale');
             }
 
             loadNextFurTextures(callback) {
@@ -161,7 +154,6 @@ define([
                     counter++;
                     if (counter == 2) {
                         root.loadingNextFur = false;
-                        console.log('loaded both new textures!');
                     }
                 });
             }
@@ -238,7 +230,6 @@ define([
                 if (!this.loadingNextFur) {
                     if (this.textureFurDiffuseNext && this.textureFurAlphaNext) {
                         this.currentPreset = FurPresets.current();
-                        console.log('unloading old textures...');
                         gl.deleteTexture(this.textureFurDiffuse);
                         gl.deleteTexture(this.textureFurAlpha);
                         this.textureFurDiffuse = this.textureFurDiffuseNext;
@@ -256,10 +247,14 @@ define([
                 gl.enable(gl.CULL_FACE);
                 gl.cullFace(gl.BACK);
 
+                gl.depthMask(false);
+                this.drawVignette(this.textureBackground);
+                gl.depthMask(true);
+
                 this.positionCamera(0.0);
                 this.setCameraFOV(0.6); // fixme multiplier should be 1.0
 
-                this.drawCubeDiffuse(0, this.textureFurDiffuse, this.currentPreset);
+                this.drawCubeDiffuse(this.textureFurDiffuse, this.currentPreset);
 
                 gl.disable(gl.CULL_FACE);
                 gl.enable(gl.BLEND);
@@ -267,39 +262,50 @@ define([
                 // gl.blendFunc(gl.SRC_ALPHA, gl.ONE); // too bright
                 gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ZERO, gl.ONE);
 
-                this.drawFur(0, this.textureFurDiffuse, this.textureFurAlpha, this.currentPreset);
+                this.drawFur(this.textureFurDiffuse, this.textureFurAlpha, this.currentPreset);
 
                 gl.disable(gl.BLEND);
             }
 
-            drawTable() {
-                this.shaderLMTable.use();
+            drawVignette(texture) {
+                this.shaderDiffuse.use();
 
-                this.setTexture2D(0, this.textureTable, this.shaderLMTable.sTexture);
-                this.setTexture2D(1, this.textureTableLM, this.shaderLMTable.sLM);
-                gl.uniform1f(this.shaderLMTable.diffuseScale, 8.0);
-                this.drawLMVBOTranslatedRotatedScaled(this.shaderLMTable, this.modelTable, 0, 0, 0, 0, 0, 0, 1, 1, 1);
+                this.setTexture2D(0, texture, this.shaderDiffuse.sTexture);
+
+                // MatrixUtils.mat4.ortho(this.matOrtho, -1, 1, -1, 1, 2.0, 250);
+
+                gl.bindBuffer(gl.ARRAY_BUFFER, this.vignette.buffer);
+
+                gl.vertexAttribPointer(this.shaderDiffuse.rm_Vertex, 3, gl.FLOAT, false, this.TRIANGLE_VERTICES_DATA_STRIDE_BYTES, this.TRIANGLE_VERTICES_DATA_POS_OFFSET * this.FLOAT_SIZE_BYTES);
+                gl.enableVertexAttribArray(this.shaderDiffuse.rm_Vertex);
+
+                gl.vertexAttribPointer(this.shaderDiffuse.rm_TexCoord0, 2, gl.FLOAT, false, this.TRIANGLE_VERTICES_DATA_STRIDE_BYTES, this.TRIANGLE_VERTICES_DATA_UV_OFFSET * this.FLOAT_SIZE_BYTES);
+                gl.enableVertexAttribArray(this.shaderDiffuse.rm_TexCoord0);
+
+                gl.uniformMatrix4fv(this.shaderDiffuse.view_proj_matrix, false, this.matOrtho);
+                gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
             }
 
-            drawCubeDiffuse(offsetY, texture, preset) {
+
+            drawCubeDiffuse(texture, preset) {
                 this.shaderDiffuseColored.use();
                 this.setTexture2D(0, texture, this.shaderDiffuseColored.sTexture);
                 gl.uniform4f(this.shaderDiffuseColored.color, preset.startColor[0], preset.startColor[1], preset.startColor[2], preset.startColor[3]);
-                this.drawDiffuseNormalStrideVBOTranslatedRotatedScaled(this.shaderDiffuseColored, this.modelCube, 0, offsetY, 0, 0, 0, this.angleYaw, 1, 1, 1);
+                this.drawDiffuseNormalStrideVBOTranslatedRotatedScaled(this.shaderDiffuseColored, this.modelCube, 0, 0, 0, 0, 0, this.angleYaw, 1, 1, 1);
             }
 
-            drawLoadingCube(offsetY) {
+            drawLoadingCube() {
                 this.shaderDiffuseColored.use();
                 this.setTexture2D(0, this.textureChecker, this.shaderDiffuseColored.sTexture);
                 gl.uniform4f(this.shaderDiffuseColored.color, 0.8, 0.8, 0.8, 1);
-                this.drawDiffuseNormalStrideVBOTranslatedRotatedScaled(this.shaderDiffuseColored, this.modelCube, 0, offsetY, 0, 0, 0, this.angleYaw, 1, 1, 1);
+                this.drawDiffuseNormalStrideVBOTranslatedRotatedScaled(this.shaderDiffuseColored, this.modelCube, 0, 0, 0, 0, 0, this.angleYaw, 1, 1, 1);
             }
 
-            drawFur(offsetY, textureDiffuse, textureAlpha, preset) {
+            drawFur(textureDiffuse, textureAlpha, preset) {
                 this.shaderFur.use();
                 this.setTexture2D(0, textureDiffuse, this.shaderFur.diffuseMap);
                 this.setTexture2D(1, textureAlpha, this.shaderFur.alphaMap);
-                this.drawFurVBOTranslatedRotatedScaledInstanced(preset, this.shaderFur, this.modelCube, 0, offsetY, 0, 0, 0, this.angleYaw, 1, 1, 1);
+                this.drawFurVBOTranslatedRotatedScaledInstanced(preset, this.shaderFur, this.modelCube, 0, 0, 0, 0, 0, this.angleYaw, 1, 1, 1);
             }
 
             drawDiffuseNormalStrideVBOTranslatedRotatedScaled(shader, model, tx, ty, tz, rx, ry, rz, sx, sy, sz) {
